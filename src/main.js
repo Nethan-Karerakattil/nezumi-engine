ffa4afdbf6c73859a1c76419e3389cd8c1674f6e
Nethan Karerakattil <nethan.karerakattil@gmail.com>
2024-04-27 19:23:51 -0400

    renderer can display 2d textures

--- src/main.js
+++ src/main.js
@@ -1,42 +1,58 @@
 const canvas = document.querySelector("#c");
 const gl = canvas.getContext("webgl");
 
-canvas.width = 256;
-canvas.height = 256;
+canvas.width = 512;
+canvas.height = 512;
 
 /* Variables */
 let scene_angle = 0;
 let scene_scale = [1, 1];
-let scene_translations = [200, 150];
+let scene_translations = [256, 256];
 
 let positions = new Float32Array([
-    -150, -100,
-    150, -100,
-    -150, 100,
-    150, -100,
-    -150, 100,
-    150, 100
+    -100, -100,
+    100, -100,
+    -100, 100,
+    100, -100,
+    -100, 100,
+    100, 100
 ]);
+
+let tex_coords = new Float32Array([
+    0.0,  0.0,
+    1.0,  0.0,
+    0.0,  1.0,
+
+    1.0,  0.0,
+    0.0,  1.0,
+    1.0,  1.0
+]);
+
+let image = new Image();
+image.src = "./src/assets/texture.jpg";
+image.onload = draw_scene;
 
 /* Shader Code */
 const vertex_shader_src = `
     attribute vec2 a_position;
-    attribute vec4 a_color;
+    attribute vec2 a_texCoord;
     uniform mat3 u_matrix;
-    varying vec4 v_color;
+    varying vec2 v_texCoord;
     
     void main(){
         gl_Position = vec4((u_matrix * vec3(a_position, 1)).xy, 0, 1);
-        v_color = a_color;
+        v_texCoord = a_texCoord;
     }
 `;
 
 const fragment_shader_src = `
     precision mediump float;
-    varying vec4 v_color;
     
-    void main(){
-        gl_FragColor = v_color;
+    uniform sampler2D u_image;
+    varying vec2 v_texCoord;
+    
+    void main() {
+        gl_FragColor = texture2D(u_image, v_texCoord);
     }
 `;
 
@@ -74,39 +90,23 @@ function create_program(gl, vertex_shader, fragment_shader){
     gl.deleteProgram();
 }
 
-function generate_color(){
-    let col_1 = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
-    let col_2 = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
-
-    return new Uint8Array([
-        ...col_1, 255,
-        ...col_1, 255,
-        ...col_1, 255,
-
-        ...col_2, 255,
-        ...col_2, 255,
-        ...col_2, 255,
-    ]);
-}
-
 /* WebGL Driver Code */
 const vertex_shader = create_shader(gl, gl.VERTEX_SHADER, vertex_shader_src);
 const fragment_shader = create_shader(gl, gl.FRAGMENT_SHADER, fragment_shader_src);
 const program = create_program(gl, vertex_shader, fragment_shader);
 
 let pos_attribute_loc = gl.getAttribLocation(program, "a_position");
-let col_attribute_loc = gl.getAttribLocation(program, "a_color");
+let tex_attribute_loc = gl.getAttribLocation(program, "a_texCoord");
 let mat_uniform_loc = gl.getUniformLocation(program, "u_matrix");
 
 let pos_buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, pos_buffer);
 gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
 
-let col_buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, col_buffer);
-gl.bufferData(gl.ARRAY_BUFFER, generate_color(), gl.STATIC_DRAW);
+let tex_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, tex_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, tex_coords, gl.STATIC_DRAW);
 
-draw_scene();
 function draw_scene(){
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.clear(gl.COLOR_BUFFER_BIT);
@@ -116,9 +116,19 @@ function draw_scene(){
     gl.bindBuffer(gl.ARRAY_BUFFER, pos_buffer);
     gl.vertexAttribPointer(pos_attribute_loc, 2, gl.FLOAT, false, 0, 0);
 
-    gl.enableVertexAttribArray(col_attribute_loc);
-    gl.bindBuffer(gl.ARRAY_BUFFER, col_buffer);
-    gl.vertexAttribPointer(col_attribute_loc, 4, gl.UNSIGNED_BYTE, true, 0, 0);
+    gl.enableVertexAttribArray(tex_attribute_loc);
+    gl.bindBuffer(gl.ARRAY_BUFFER, tex_buffer);
+    gl.vertexAttribPointer(tex_attribute_loc, 2, gl.FLOAT, false, 0, 0);
+
+    let texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 
     let mat = m3.projection(gl.canvas.clientWidth, gl.canvas.clientHeight);
     mat = m3.translate(mat, scene_translations[0], scene_translations[1]);
